200e5da1fa7ea40762335450808e9f0a1c00d636
C-C-Burrows <99225568+C-C-Burrows@users.noreply.github.com>
2022-10-08 12:18:14 +1100

    Update RMS.ino
    Cleaned up setup and added in Temp Includes and setup

--- RMS/RMS.ino
+++ RMS/RMS.ino
@@ -11,6 +11,25 @@
 AsyncWebServer server(80);
 
 
+//Temperature Includes and setup
+#include <Wire.h>
+#include "Adafruit_ADT7410.h"
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include "Adafruit_miniTFTWing.h"
+
+
+Adafruit_miniTFTWing ss;
+#define TFT_RST    -1    // we use the seesaw for resetting to save a pin
+#define TFT_CS   14
+#define TFT_DC   32
+
+// Create the ADT7410 temperature sensor object
+Adafruit_ADT7410 tempsensor = Adafruit_ADT7410();
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+
+
 // RTC Start - Remove if unnecessary
 #include "RTClib.h"
 
@@ -24,6 +43,82 @@ boolean LEDOn = false; // State of Built-in LED true=on, false=off.
 
 
 void setup() {
+  temperatureSetup();
+  spiffWifiSetup();
+}
+
+void loop() {
+  builtinLED();
+  delay(LOOPDELAY); // To allow time to publish new code.
+}
+
+
+void builtinLED() {
+  if (LEDOn) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+
+void logEvent(String dataToLog) {
+  /*
+     Log entries to a file stored in SPIFFS partition on the ESP32.
+  */
+  // Get the updated/current time
+  DateTime rightNow = rtc.now();
+  char csvReadableDate[25];
+  sprintf(csvReadableDate, "%02d,%02d,%02d,%02d,%02d,%02d,",  rightNow.year(), rightNow.month(), rightNow.day(), rightNow.hour(), rightNow.minute(), rightNow.second());
+
+  String logTemp = csvReadableDate + dataToLog + "\n"; // Add the data to log onto the end of the date/time
+
+  const char * logEntry = logTemp.c_str(); //convert the logtemp to a char * variable
+
+  //Add the log entry to the end of logevents.csv
+  appendFile(SPIFFS, "/logEvents.csv", logEntry);
+
+  // Output the logEvents - FOR DEBUG ONLY. Comment out to avoid spamming the serial monitor.
+  //  readFile(SPIFFS, "/logEvents.csv");
+
+  Serial.print("\nEvent Logged: ");
+  Serial.println(logEntry);
+}
+
+void temperatureSetup()
+{
+  Serial.begin(9600);
+
+  if (!ss.begin()) {
+    Serial.println("seesaw init error!");
+    while (1);
+  }
+  else Serial.println("seesaw started");
+
+  ss.tftReset();
+  ss.setBacklight(0x0); //set the backlight fully on
+
+  // Use this initializer (uncomment) if you're using a 0.96" 180x60 TFT
+  tft.initR(INITR_MINI160x80);   // initialize a ST7735S chip, mini display
+
+  tft.setRotation(3);
+  tft.fillScreen(ST77XX_BLACK);
+
+
+  Serial.println("ADT7410 demo");
+
+  // Make sure the sensor is found, you can also pass in a different i2c
+  // address with tempsensor.begin(0x49) for example
+  if (!tempsensor.begin()) {
+    Serial.println("Couldn't find ADT7410!");
+    while (1);
+  }
+
+  // sensor takes 250 ms to get first readings
+  delay(250);
+}
+
+void spiffWifiSetup()
+{
   Serial.begin(9600);
   while (!Serial) {
     delay(10);
@@ -64,44 +159,4 @@ void setup() {
   rtc.start();
   pinMode(LED_BUILTIN, OUTPUT);
 
-}
-
-void loop() {
-
-  builtinLED();
-
-  //This is a test change
-  delay(LOOPDELAY); // To allow time to publish new code.
-}
-
-
-void builtinLED() {
-  if (LEDOn) {
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
-  }
-}
-
-void logEvent(String dataToLog) {
-  /*
-     Log entries to a file stored in SPIFFS partition on the ESP32.
-  */
-  // Get the updated/current time
-  DateTime rightNow = rtc.now();
-  char csvReadableDate[25];
-  sprintf(csvReadableDate, "%02d,%02d,%02d,%02d,%02d,%02d,",  rightNow.year(), rightNow.month(), rightNow.day(), rightNow.hour(), rightNow.minute(), rightNow.second());
-
-  String logTemp = csvReadableDate + dataToLog + "\n"; // Add the data to log onto the end of the date/time
-
-  const char * logEntry = logTemp.c_str(); //convert the logtemp to a char * variable
-
-  //Add the log entry to the end of logevents.csv
-  appendFile(SPIFFS, "/logEvents.csv", logEntry);
-
-  // Output the logEvents - FOR DEBUG ONLY. Comment out to avoid spamming the serial monitor.
-  //  readFile(SPIFFS, "/logEvents.csv");
-
-  Serial.print("\nEvent Logged: ");
-  Serial.println(logEntry);
 }
