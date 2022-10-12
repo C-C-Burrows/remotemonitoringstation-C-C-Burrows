e07c8eba8e6384ce0d6fed46ae0c27037be58a4e
C-C-Burrows <99225568+C-C-Burrows@users.noreply.github.com>
2022-10-12 17:00:30 +1100

    Lastest version of build

--- RMS/build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ RMS/build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -424,7 +424,7 @@ void spiffWifiSetup()
   }
 
   // The following line can be uncommented if the time needs to be reset.
-  rtc.adjust(DateTime(((reinterpret_cast<const __FlashStringHelper *>(("Oct 11 2022")))), ((reinterpret_cast<const __FlashStringHelper *>(("11:37:08"))))));
+  rtc.adjust(DateTime(((reinterpret_cast<const __FlashStringHelper *>(("Oct 12 2022")))), ((reinterpret_cast<const __FlashStringHelper *>(("16:54:48"))))));
   rtc.start();
   pinMode(LED_BUILTIN, 0x03);
 }
@@ -546,73 +546,73 @@ void deleteFile(fs::FS &fs, const char * path) {
   }
 }
 # 1 "c:\\Users\\CharlotteBurrows\\OneDrive - Friends and Family Dogfood ozburrows.com\\Documents\\GitHub\\remotemonitoringstation-C-C-Burrows\\RMS\\websiteFunctionality.ino"
-void routesConfiguration()
-{
-  // this is used to configure the router
-  server.onNotFound([](AsyncWebServerRequest *request)
-                    { request->send(SPIFFS, "/404.html"); });
+void routesConfiguration() {
+
   // Example of a 'standard' route
   // No Authentication
-  server.on("/index.html", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
+  server.on("/index.html", HTTP_GET, [](AsyncWebServerRequest * request) {
     logEvent("route: /");
-    request->send(SPIFFS, "/index.html", "text/html"); });
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
 
   // Duplicated serving of index.html route, so the IP can be entered directly to browser.
   // No Authentication
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     logEvent("route: /");
-    request->send(SPIFFS, "/index.html", "text/html"); });
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
 
   // Example of linking to an external file
-  server.on("/arduino.css", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(SPIFFS, "/arduino.css", "text/css"); });
+  server.on("/arduino.css", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/arduino.css", "text/css");
+  });
+
 
   // Example of a route with additional authentication (popup in browser)
   // And uses the processor function.
-  server.on("/dashboard.html", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
+  server.on("/dashboard.html", HTTP_GET, [](AsyncWebServerRequest * request) {
     if (!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
     logEvent("Dashboard");
-    request->send(SPIFFS, "/dashboard.html", "text/html", false, processor); });
+    request->send(SPIFFS, "/dashboard.html", "text/html", false, processor);
+  });
+
 
   // Example of route with authentication, and use of processor
   // Also demonstrates how to have arduino functionality included (turn LED on)
-  server.on("/LEDOn", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
+  server.on("/LEDOn", HTTP_GET, [](AsyncWebServerRequest * request) {
     if (!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
     LEDOn = true;
-    request->send(SPIFFS, "/dashboard.html", "text/html", false, processor); });
+    request->send(SPIFFS, "/dashboard.html", "text/html", false, processor);
+  });
 
-  server.on("/LEDOff", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
+
+  server.on("/LEDOff", HTTP_GET, [](AsyncWebServerRequest * request) {
     if (!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
     LEDOn = false;
-    request->send(SPIFFS, "/dashboard.html", "text/html", false, processor); });
+    request->send(SPIFFS, "/dashboard.html", "text/html", false, processor);
+  });
+
 
   // Example of route which sets file to download - 'true' in send() command.
-  server.on("/logOutput", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
+  server.on("/logOutput", HTTP_GET, [](AsyncWebServerRequest * request) {
     if (!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
     logEvent("Log Event Download");
-    request->send(SPIFFS, "/logEvents.csv", "text/html", true); });
+    request->send(SPIFFS, "/logEvents.csv", "text/html", true);
+  });
 }
 
-String getDateTime()
-{
+String getDateTime() {
   DateTime rightNow = rtc.now();
   char csvReadableDate[25];
   sprintf(csvReadableDate, "%02d:%02d:%02d %02d/%02d/%02d", rightNow.hour(), rightNow.minute(), rightNow.second(), rightNow.day(), rightNow.month(), rightNow.year());
   return csvReadableDate;
 }
 
-String processor(const String &var)
-{
+String processor(const String& var) {
   /*
 
      Updates the HTML by replacing set variables with return value from here.
@@ -627,8 +627,7 @@ String processor(const String &var)
 
   */
 # 76 "c:\\Users\\CharlotteBurrows\\OneDrive - Friends and Family Dogfood ozburrows.com\\Documents\\GitHub\\remotemonitoringstation-C-C-Burrows\\RMS\\websiteFunctionality.ino"
-  if (var == "DATETIME")
-  {
+  if (var == "DATETIME") {
     String datetime = getDateTime();
     return datetime;
   }
