3a9e1aa101e2951e41c2bfdb5f9c67a2ee6c08c6
C-C-Burrows <99225568+C-C-Burrows@users.noreply.github.com>
2022-11-12 09:55:32 +1100

    BETTER COLOR SCEEM
    BETTER COLOR SCEEM

--- RMS/build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ RMS/build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -498,7 +498,7 @@ void spiffWifiSetup()
   }
 
   // The following line can be uncommented if the time needs to be reset.
-  rtc.adjust(DateTime(((reinterpret_cast<const __FlashStringHelper *>(("Nov  6 2022")))), ((reinterpret_cast<const __FlashStringHelper *>(("10:12:34"))))));
+  rtc.adjust(DateTime(((reinterpret_cast<const __FlashStringHelper *>(("Nov 12 2022")))), ((reinterpret_cast<const __FlashStringHelper *>(("09:51:33"))))));
   rtc.start();
   pinMode(LED_BUILTIN, 0x03);
 }
